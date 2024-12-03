b9c6293932420551c6b960f55e428fa3fa62156a
IrineCKimeto <irenekimetoc@gmail.com>
2024-12-03 15:14:25 +0300

    lib/data_structures.py

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,4 @@
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -16,16 +17,20 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
     return [food['name'] for food in spicy_foods]
 
+
 def get_spiciest_foods(spicy_foods):
     return [food for food in spicy_foods if food['heat_level'] > 5]
+
 
 def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
         heat_level_emojis = "🌶" * food['heat_level']
         print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for food in spicy_foods:
@@ -33,14 +38,17 @@ def get_spicy_food_by_cuisine(spicy_foods, cuisine):
             return food
     return None
 
+
 def print_spiciest_foods(spicy_foods):
     spiciest_foods = get_spiciest_foods(spicy_foods)
     print_spicy_foods(spiciest_foods)
+
 
 def get_average_heat_level(spicy_foods):
     total_heat_level = sum(food['heat_level'] for food in spicy_foods)
     return total_heat_level // len(spicy_foods)
 
+
 def create_spicy_food(spicy_foods, spicy_food):
-    spicy_foods.append(new_spicy_food)
+    spicy_foods.append(spicy_food) 
     return spicy_foods
